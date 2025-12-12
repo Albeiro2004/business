8e37f19dee75976cd72861727532f0234f9df9fb
albeiroluis <ahoyosgonzalez25@correo.unicordoba.edu.co>
2025-12-12 03:05:14 -0500

    1er incremento

--- app/main.py
+++ app/main.py
@@ -20,9 +20,14 @@ app = FastAPI(
     lifespan=lifespan
 )
 
+origins = [
+    "https://gestor-negocios-frontend.vercel.app",
+    "http://localhost:4200"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
